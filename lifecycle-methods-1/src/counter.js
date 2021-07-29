9a2dac6f89dbfabd279404d7c0f1d5b43bc4c53f
ashraffares <ashraffares090@gmail.com>
2021-07-29 15:49:32 +0200

    Use setState method to update the state

--- lifecycle-methods-1/src/counter.js
+++ lifecycle-methods-1/src/counter.js
@@ -9,9 +9,9 @@ export default class Counter extends React.Component {
             counter: 0,
         }
 
-        this.increment = () => this.state.counter + 1;
-        this.decrement = () => this.state.counter - 1;
-        this.reset = () => this.state.counter = 0;
+        this.increment = () => this.setState({counter: this.state.counter + 1});
+        this.decrement = () => this.setState({counter: this.state.counter - 1});
+        this.reset = () => this.setState({counter: 0});
     }
     render() {
         console.log('Start Render');
