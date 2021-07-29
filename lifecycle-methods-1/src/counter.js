dac3b6a0e87374f44c556eda9a2ef5e156a8b131
ashraffares <ashraffares090@gmail.com>
2021-07-29 15:47:01 +0200

    Add function to increment-decrement-reset for counter class

--- lifecycle-methods-1/src/counter.js
+++ lifecycle-methods-1/src/counter.js
@@ -2,17 +2,29 @@ import React from 'react'
 
 export default class Counter extends React.Component {
     constructor(props) {
+        console.log('Start Constructor')
         super(props);
 
         this.state = {
             counter: 0,
         }
+
+        this.increment = () => this.state.counter + 1;
+        this.decrement = () => this.state.counter - 1;
+        this.reset = () => this.state.counter = 0;
     }
     render() {
+        console.log('Start Render');
         return (
-            <div className="counter">
-                 Counter: {this.state.counter}
-            </div>
+           <div>
+                <button onClick={this.increment}>Increment</button>
+                <button onClick={this.decrement}>Decrement</button>
+                <button onClick={this.reset}>Reset</button>
+
+                <div className="counter">
+                    Counter: {this.state.counter}
+                </div>
+           </div>
         )
     }
 }
