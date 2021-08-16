0200a577b11d4e97ebff334d87909e319bcc7a13
ashraffares <ashraffares090@gmail.com>
2021-08-16 17:26:57 +0200

    add state to Square component

--- tictactoe/src/component/Board/Board.js
+++ tictactoe/src/component/Board/Board.js
@@ -4,7 +4,7 @@ import styles from "./Board.module.css";
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
--- tictactoe/src/component/Square/Square.js
+++ tictactoe/src/component/Square/Square.js
@@ -2,10 +2,18 @@ import React from 'react';
 import styles from './Square.module.css'
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
     render() {
       return (
-        <button className={styles.square}>
-          {/* TODO */}
+        <button className={styles.square} 
+        onClick={() => this.setState({value: 'X'})}
+        >
+          {this.state.value}
         </button>
       );
     }
