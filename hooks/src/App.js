88e4628c246378e732f4d5133246dd61a6e38096
ashraffares <ashraffares090@gmail.com>
2021-08-22 12:52:08 +0200

    update app

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import Effect from "./component/useeffect";
+import State from './component/usestate';
 import './App.css';
 
 
-const App = () => <Effect />
+const App = () => {
+    return (
+        <>
+            <Effect />
+            <State />
+        </>
+    )
+}
 
 export default App;
